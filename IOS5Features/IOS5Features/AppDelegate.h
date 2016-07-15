518e797f0896f7d8bd0b30484676640c116beab9
karthik <k.raghunath@globallogic.com>
2016-07-15 16:18:51 -0500

    changed to objective c code.

---
+++ IOS5Features/IOS5Features/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS5Features
+//
+//  Created by karthik on 7/15/16.
+//  Copyright © 2016 KITE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS5Features/IOS5Features/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS5Features
+//
+//  Created by karthik on 7/15/16.
+//  Copyright © 2016 KITE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
